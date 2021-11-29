c75bacb4199d5d3fc679d8c041b04ea659d09949
Romwil <romwil.pilapil@gmail.com>
2021-11-30 03:00:54 +0800

    update code in py_gen_map.py

--- py_gen_map.py
+++ py_gen_map.py
@@ -1,4 +1,8 @@
 import folium
 
-map = folium.Map(location=[14.586250, 120.999935])
+map = folium.Map(location=[14.586250, 120.999935], zoom_start=6, tiles="Stamen Terrain")
+fg = folium.FeatureGroup(name="My map")
+fg.add_child(folium.Marker(location=[14.586250, 120.999935], popup="marker pointer", icon=folium.Icon(color="blue")))
+map.add_child(fg)
+
 map.save("pandacan_bbac.html")
